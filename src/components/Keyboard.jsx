e12dbc10e993343e75318eb7297d7b420e8977bf
Rachel Sanham <green_chul@hotmail.com>
2022-08-26 17:07:22 +0100

    added answers and fixed bug when loading app

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,10 +1,15 @@
-const Keyboard = ({ question, setAnswer }) => {
+import { useRef } from "react";
+
+const Keyboard = ({ setAnswer }) => {
+  const inputRef = useRef(null);
   const handleClick = () => {
     console.log("calling handleClick");
+    console.log(inputRef.current.value);
+    setAnswer(inputRef.current.value);
   };
   return (
     <div>
-      <input />
+      <input type="number" ref={inputRef} />
       <button onClick={handleClick}>Submit</button>
     </div>
   );
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,21 +2,31 @@ import Questions from "./Questions";
 import { useState, useEffect } from "react";
 import generateQuestion from "../utils/generateQuestion";
 import Keyboard from "./Keyboard";
+import checkAnswer from "../utils/checkAnswer";
 
 const Quiz = () => {
   const [question, setQuestion] = useState();
   const [answer, setAnswer] = useState();
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     const response = generateQuestion();
     setQuestion(response);
-  }, []);
+  }, [score]);
+
+  useEffect(() => {
+    if (question) {
+      console.log("New answer submitted", answer);
+      console.log("answer is", checkAnswer(question.answer, Number(answer)));
+    }
+  }, [answer]);
 
   return (
     <div>
       QUESTIONS
+      <p>Score: {score}</p>
       {question && <Questions question={question} />}
-      {question && <Keyboard question={question} setAnswer={setAnswer} />}
+      <Keyboard setAnswer={setAnswer} />
     </div>
   );
 };
