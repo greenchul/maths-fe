c4006b573646b8cca5538f2c3c48700a6030a422
Rachel Sanham <green_chul@hotmail.com>
2022-08-30 16:24:22 +0100

    added pressing enter to trigger answer submission in keyboard component

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,13 +1,30 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 const Keyboard = ({ setAnswer }) => {
   const inputRef = useRef(null);
+
   const handleClick = () => {
     console.log("calling handleClick");
     console.log(inputRef.current.value);
     setAnswer(inputRef.current.value);
     inputRef.current.value = null;
   };
+
+  const handleKeyPress = (e) => {
+    console.log(e);
+    if (e.keyCode === 13) {
+      console.log("you have pressed enter");
+      setAnswer(inputRef.current.value);
+      inputRef.current.value = null;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyPress);
+    return () => {
+      window.removeEventListener("keyup", handleKeyPress);
+    };
+  }, []);
   return (
     <div>
       <input type="number" ref={inputRef} placeholder="answer" />
